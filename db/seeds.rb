cd4ec87d6909e0a38c740088e0d1ea97110916c0
kiburei <akiburei@gmail.com>
2017-11-10 15:49:59 +0300

    add model blog and seed db with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times { BlogPost.create!(title: "The Paradise Papers Primer: Trump-Russia Links And Global-Scale Corruption",
+                            content: "In early 2016, a small German newspaper received an unprecedented leak of 11.5 million files from the world’s fourth largest offshore law firm. These documents, and the revelations they held would come to be known as the Panama Papers — a name now infamous for shedding light on the multitude of ways the rich and powerful exploit offshore tax havens for their own benefit.
+
+This Sunday, an even more expansive leak of files was made public by the same organizations that brought us the Panama Papers. These 13.4 million files, once again obtained by German newspaper Süddeutsche Zeitung, and presented by the International Consortium of Investigative Journalists (ICIJ), expose exactly how deeply offshore financial dealings are tied with many of the world’s most powerful political players.
+
+From Jared Kushner, to Trump’s commerce secretary Wilbur Ross, to Russian financial investments in Facebook and Twitter, the newly released Paradise Papers are filled to the brim with enough political corruption and shady dealings to keep the spy-movie genre going for centuries.
+
+So what exactly do these 13.4 million documents reveal, and what does it mean for the American public? In this deep dive, we’ll explore what these files actually are, the key players involved, and the most important revelations.
+
+Sit back, relax, and let’s deconstruct the Paradise Papers.
+
+Essentially the Paradise Papers are comprised of files from two offshore service providers (companies that are incorporated under the laws of a different country than they would be assumed to reside, often used for tax breaks or looser regulations), and the registries of 19 companies that act as tax havens for businesses.
+
+They contain nearly, “7 million loan agreements, financial statements, emails, trust deeds and other paperwork from nearly 50 years at Appleby, a leading offshore law firm with offices in Bermuda and beyond,” according to the organization that released the files.
+
+As mentioned above, these documents were obtained by a German newspaper and disseminated by the ICIJ, an international organization that is comprised of more than 200 investigative journalists in 70 countries. In this specific investigation, they worked with over 380 journalists stationed all over the world. The ICIJ also partnered with many mainstream news organizations, such as The Guardian and The New York Times, in their release of the files.
+
+Within the Paradise Papers lie many revelations, but generally speaking these files shed light on the following key findings:
+
+    Many members of President Donald Trump’s inner circle have connections with extensive offshore dealings. Most notable is a relationship between US commerce secretary, Wilbur Ross and Russian President Vladimir Putin’s family, and links between Kremlin-tied businesses and a close associate of Jared Kushner.
+    More than a dozen Trump Cabinet members, advisers, and important donors appear in the documents.
+    Twitter and Facebook received investments in the hundreds of millions of dollars which can be traced back to two Russian state institutions.
+    The Queen of England’s private estate invested millions of pounds in an offshore fund on the Cayman Islands.
+    Canadian prime minister Justin Trudeau’s chief fundraiser and senior adviser managed a tax-avoiding Cayman Islands trust.
+    Both Nike and Apple alleged attempted to take advantage of offshore havens.
+
+The leaks expose how the worlds of powerful political players, influential corporations, and global entities conver") }
